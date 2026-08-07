55376360f75414772f4383df81827e5b1185465d
cuentafamosa534-a11y <cuentafamosa534@gmail.com>
2026-08-07 18:58:29 -0400

    Remove products from products.js
    Removed multiple products from the products list.

--- products.js
+++ products.js
@@ -8,13 +8,6 @@ const PRODUCTS = [
     image: "https://images.unsplash.com/photo-1606220588913-b3aacb4d2f46?w=600"
 },
 
-{
-    id: 2,
-    name: "Cargador USB-C 20W",
-    price: 13000,
-    category: "Cargadores",
-    image: "https://images.unsplash.com/photo-1583863788434-e58a36330cf0?w=600"
-},
 
 {
     id: 3,
@@ -24,21 +17,7 @@ const PRODUCTS = [
     image: "https://images.unsplash.com/photo-1615526675159-e248c3021d3f?w=600"
 },
 
-{
-    id: 4,
-    name: "Power Bank 10.000 mAh",
-    price: 25000,
-    category: "Power Bank",
-    image: "https://images.unsplash.com/photo-1587033411391-5d9e51cce126?w=600"
-},
 
-{
-    id: 5,
-    name: "Cargador USB-C 25W",
-    price: 18000,
-    category: "Cargadores",
-    image: "https://images.unsplash.com/photo-1583863788434-e58a36330cf0?w=600"
-},
 
 {
     id: 6,
